a99b85d9de914a15fec5ca37c0f53e668707fe93
aceek <linhardtilan@gmail.com>
2023-03-12 01:11:59 +0100

    correction leak check caract

--- srcs/parser/ft_parsing2.c
+++ srcs/parser/ft_parsing2.c
@@ -6,7 +6,7 @@
 /*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 00:37:38 by ilinhard          #+#    #+#             */
-/*   Updated: 2023/03/12 00:24:01 by ilinhard         ###   ########.fr       */
+/*   Updated: 2023/03/12 01:11:38 by ilinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,26 @@ int	ft_check_carac(char *str, int *count, t_game *game)
 	return (0);
 }
 
-char	**ft_final_map(char **map, int start_map, int len)
+char    **ft_final_map(char **map, int start_map, int len)
 {
-	char	**final_map;
-	int		i;
+    char    **final_map;
+    int        i;
 
-	i = 0;
-	final_map = NULL;
-	len += 1;
-	if (len < 3)
-		return (ft_free_tab(map), NULL);
-	final_map = malloc(sizeof(char *) * (len + 1));
-	if (!final_map)
-		return (ft_free_tab(map), NULL);
-	while (map[start_map + i])
-	{
-		final_map[i] = ft_strdup(map[start_map + i]);
-		i++;
-	}
-	final_map[i] = NULL;
-	ft_free_tab(map);
-	return (final_map);
+    i = 0;
+    final_map = NULL;
+    if (len < 3)
+        return (ft_free_tab(map), NULL);
+    final_map = malloc(sizeof(char *) * (len + 1));
+    if (!final_map)
+        return (ft_free_tab(map), NULL);
+    while (map[start_map + i])
+    {
+        final_map[i] = ft_strdup(map[start_map + i]);
+        i++;
+    }
+    final_map[i] = NULL;
+    ft_free_tab(map);
+    return (final_map);
 }
 
 void	ft_verify_map_exit(char **map, int i, int j, int *exit)
@@ -80,8 +79,9 @@ char	**ft_init_create_map(char **map, t_game *game, int i, int start_map)
 {
 	int	exit;
 
+	
 	exit = 0;
-	map = ft_final_map(map, start_map, (i - start_map - 1));
+	map = ft_final_map(map, start_map, (i - start_map));
 	if (!map)
 		return (error_handler(NULL, ERR_MAP_FORMAT), NULL);
 	map[game->player_y][game->player_x] = '0';
@@ -89,28 +89,35 @@ char	**ft_init_create_map(char **map, t_game *game, int i, int start_map)
 	if (exit)
 		return (ft_free_tab(map), error_handler(NULL, ERR_MAP_EXIT), NULL);
 	return (map);
+
 }
 
-char	**ft_create_map(char **map, t_game *game)
+char    **ft_create_map(char **map, t_game *game)
 {
-	int	i;
-	int	start_map;
-	int	count;
+    int    i;
+    int    start_map;
+    int    count;
 
-	i = 0;
-	count = 0;
-	ft_search_tab(map, "1", &i, 0);
-	start_map = i - 1;
-	while (i >= 0 && map[i])
-	{
-		if (ft_check_carac(map[i], &count, game))
-			return (ft_free_tab(map), error_handler(NULL, ERR_MAP_CARAC), NULL);
-		if (count && !game->player_y)
-			game->player_y = i - start_map;
+    i = 0;
+    count = 0;
+    ft_search_tab(map, "1", &i, 0);
+    start_map = i - 1;
+    while (i >= 0 && map[i])
+    {
+        if (ft_check_carac(map[i], &count, game))
+            return (ft_free_tab(map), error_handler(NULL, ERR_MAP_CARAC), NULL);
+        if (count && !game->player_y)
+            game->player_y = i - start_map;
 		i++;
-	}
-	if (count != 1)
-		return (ft_free_tab(map), error_handler(NULL, ERR_MAP_FORMAT), NULL);
-	map = ft_init_create_map(map, game, i, start_map);
-	return (map);
+    }
+    if (count != 1)
+        return (ft_free_tab(map),
+            error_handler(NULL, ERR_MAP_FORMAT), NULL);
+    map = ft_final_map(map, start_map, (i - start_map));
+    count = 0;
+    map[game->player_y][game->player_x] = '0';
+    ft_verify_map_exit(map, game->player_y, game->player_x, &count);
+    if (count)
+        return (ft_free_tab(map), error_handler(NULL, ERR_MAP_EXIT), NULL);
+    return (map);
 }
